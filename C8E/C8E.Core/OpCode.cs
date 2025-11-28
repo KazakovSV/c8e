1bd37a393bb805e18356ff6764c9cce3d53cd027
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-29 01:15:42 +0300

    feat: add OpCode struct for instruction decoding

---
+++ C8E/C8E.Core/OpCode.cs
@@ -0,0 +1,18 @@
+﻿namespace C8E.Core;
+
+internal readonly struct OpCode(ushort raw)
+{
+    public byte Category => (byte)(raw >> 12);
+
+    public ushort NNN => (ushort)(raw & 0x0FFF);
+
+    public byte NN => (byte)(raw & 0x00FF);
+
+    public byte N => (byte)(raw & 0x000F);
+
+    public byte X => (byte)((raw & 0x0F00) >> 8);
+
+    public byte Y => (byte)((raw & 0x00F0) >> 4);
+
+    public override string ToString() => $"0x{raw:X4}";
+}
