a4ae49357036c23e35440a4d5dc4f9d6c7ca39d7
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-28 22:57:01 +0300

    chore: add Constants.cs file
    Created Constants.cs to store emulator-wide values

---
+++ C8E/C8E.Core/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace C8E.Core;
+
+internal class Constants
+{
+    public const int DisplayWidth = 64;
+    public const int DisplayHeight = 32;
+    public const int MemorySize = 4096;
+    public const int StackSize = 16;
+    public const int RegistersCount = 16;
+    public const int ProgramStartAddress = 0x200;
+}
