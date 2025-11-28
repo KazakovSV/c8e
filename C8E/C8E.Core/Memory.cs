1451483e35007d60c1da3a25642aa8b85e5149ae
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-29 00:43:45 +0300

    feat: implement Memory class

---
+++ C8E/C8E.Core/Memory.cs
@@ -0,0 +1,54 @@
+﻿namespace C8E.Core;
+
+internal sealed class Memory
+{
+    public const int DefaultSize = 4096;
+
+    private readonly byte[] _data;
+
+    public Memory(int size = DefaultSize)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(size);
+
+        _data = new byte[size];
+    }
+
+    public byte this[int address]
+    {
+        get
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(address, _data.Length);
+
+            return _data[address];
+        }
+        set
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(address, _data.Length);
+
+            _data[address] = value;
+        }
+    }
+
+    public ushort ReadOpCode(int address)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(address);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(address, _data.Length - 1);
+
+        return (ushort)((_data[address] << 8) | _data[address + 1]);
+    }
+
+    public void Load(byte[] rom, int address)
+    {
+        ArgumentNullException.ThrowIfNull(rom);
+
+        if (rom.Length == 0)
+            throw new ArgumentException("ROM is empty.", nameof(rom));
+
+        ArgumentOutOfRangeException.ThrowIfNegative(address);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(address, _data.Length - rom.Length);
+
+        Array.Copy(rom, 0, _data, address, rom.Length);
+    }
+
+    public void Reset() => Array.Clear(_data);
+}
